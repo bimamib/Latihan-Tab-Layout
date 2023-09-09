26e0c9b660af8876024e05a2c93806b9ce48c670
bimamib <bimaprast1601@gmail.com>
2023-09-10 02:32:50 +0700

    mengubah kode di dalam SectionsPagerAdapter

--- app/src/main/java/com/bima/mytablayout/SectionsPagerAdapter.kt
+++ app/src/main/java/com/bima/mytablayout/SectionsPagerAdapter.kt
@@ -5,11 +5,17 @@ import androidx.fragment.app.Fragment
 import androidx.viewpager2.adapter.FragmentStateAdapter
 
 class SectionsPagerAdapter(activity: AppCompatActivity) : FragmentStateAdapter(activity) {
-    override fun getItemCount(): Int {
-        TODO("Not yet implemented")
+    override fun createFragment(position: Int): Fragment {
+        var fragment: Fragment? = null
+        when (position) {
+            0 -> fragment = HomeFragment()
+            1 -> fragment = ProfileFragment()
+        }
+        return fragment as Fragment
     }
 
-    override fun createFragment(position: Int): Fragment {
-        TODO("Not yet implemented")
+    override fun getItemCount(): Int {
+        return 2
     }
+
 }
